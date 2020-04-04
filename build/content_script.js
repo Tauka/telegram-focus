bb3470affa953230020e800df0ab816834c874fd
Tauyekel Kunzhol <tauyekel.kunzhol@targetai.kz>
2020-04-04 08:19:56 +0600

    bump changes 2

---
+++ build/content_script.js
@@ -0,0 +1,40 @@
+let MAX_SCROLLS = 20;
+
+const removeDialogs = () =>
+{
+	setTimeout(() =>
+	{
+		if(MAX_SCROLLS > 0)
+		{
+			document.querySelector(".im_dialogs_scrollable_wrap")
+				.scrollTop = 1000;
+
+			MAX_SCROLLS--;
+		}
+
+		setTimeout(() =>
+		{
+			chrome.storage.sync.get({
+				whitelistItems: []
+			}, function({ whitelistItems }) {
+				if(whitelistItems.length === 0)
+					return
+
+				const dialogs = document.querySelectorAll(".im_dialog");
+
+				Array.prototype.forEach.call(dialogs, node =>
+				{
+					const nameSpan = node.querySelector(".im_dialog_peer span")
+					if(whitelistItems.some(item => item === nameSpan.textContent))
+						return;
+
+					node.parentNode.removeChild(node);
+				});
+
+				window.requestIdleCallback(removeDialogs)
+			});
+		}, 100)
+	}, 0)
+}
+
+removeDialogs();
